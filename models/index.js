372584ada1ece488b7ede3003a28e45ac24bd2a6
Javier Farre <jfarre90@gmail.com>
2019-04-15 15:22:53 +0000

    updated links to work when deployed to heroku

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.set('debug', true);
 
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/wishlist', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/wishlist', {
     useNewUrlParser: true,
     keepAlive: true
 });
